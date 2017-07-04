87678926c4368fb9f68ac65c069236f2a96b27fe
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-07-04 16:19:26 -0300

    v10: add code from superbowleto

--- dist/handler.js
+++ dist/handler.js
@@ -1,8 +1,45 @@
 const Credstash = require('nodecredstash')
 
-const VERSION = 9
+const VERSION = 10
 
 process.env.STAGE = 'sandbox'
+
+module.exports = {
+  migrate: migrate,
+  hello: hello
+}
+
+function migrate (event, context, callback) {
+  console.log('VERSION->', VERSION)
+
+  return Promise.resolve()
+    .then(getDatabasePassword)
+    .then(x => {
+      console.log('Success')
+      console.log(x)
+      callback(null, x)
+    })
+    .catch(x => {
+      console.log('Error')
+      console.log(x)
+      callback(x)
+    })
+};
+
+function getDatabasePassword () {
+  const Credstash = require('nodecredstash')
+
+  const credstash = Credstash({
+    table: 'credential-store',
+    awsOpts: { region: 'us-east-1' }
+  })
+
+  return credstash.getSecret({
+    name: `${process.env.STAGE}/database/password`,
+    version: 1,
+    context: {}
+  })
+}
 
 function hello (event, context, callback) {
   console.log('VERSION', VERSION)
@@ -26,8 +63,3 @@ function hello (event, context, callback) {
       callback(x)
     })
 }
-
-module.exports = {
-  hello: hello
-}
-
