73e755f7c21e5b4a5be2adb2d82f88b1a1411064
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-07-04 13:25:14 -0300

    v1: simple credstash deployed with deployer

--- dist/handler.js
+++ dist/handler.js
@@ -1,16 +1,29 @@
-module.exports = {
-  hello: hello,
-  fizz: fizz
-}
+const Credstash = require('nodecredstash')
+
+process.env.STAGE = 'sandbox'
 
 function hello (event, context, callback) {
-  callback(null, {
-    message: "Hello World by Deployer!"
+  const credstash = new Credstash({
+    table: 'credential-store',
+    awsOpts: { region: 'us-east-1' }
   })
+
+  console.log('before getSecret')
+
+  credstash.getSecret({
+    name: `${process.env.STAGE}/database/password`
+  })
+    .then((x) => {
+      console.log('after getSecret', x)
+      callback(null, x)
+    })
+    .catch((x) => {
+      console.log('catch getSecret', x)
+      callback(x)
+    })
 }
 
-function fizz (event, context, callback) {
-  callback(null, {
-    message: "Fizz Buzz by Deployer!"
-  })
+module.exports = {
+  hello: hello
 }
+
